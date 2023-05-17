e29bdca297d63c40e98d089fdb8f0ff75d52c47e
Ashish Verma <av84770@gmail.com>
2023-05-18 02:40:21 +0530

    new changes

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,11 +2,26 @@
 const logs = require("../common/logs.common");
 // calling logic function
 const { getVideoM, postVideoM } = require("../models/video.model");
-
+const ApiFeatures = require("../utils/apiFeatures");
+const VideoModel = require("../db/schema/video.schema");
 // test get
 async function getVideoC(req, res) {
-  const result = await getVideoM(req.body);
-  return res.json(result).status(200);
+  // console.log(req.body);
+  let resultPerPage = 6;
+  const apiFeatures = new ApiFeatures(
+    VideoModel.find({}),
+    req.query
+  ).pagination(resultPerPage);
+  const result = await apiFeatures.query;
+
+  // const result = await getVideoM(req.body);
+  return res
+    .json({
+      message: result,
+      success: true,
+      token: null,
+    })
+    .status(200);
 }
 async function postVideoC(req, res) {
   const result = await postVideoM(req.body);
